2ba7e87ae5617d7e4cc81cd06351a5e288b1b64e
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2021-01-12 16:15:58 +0300

    Login page

--- FarmTracker/FarmTracker/MasterDetailPage1Master.xaml.cs
+++ FarmTracker/FarmTracker/MasterDetailPage1Master.xaml.cs
@@ -36,6 +36,7 @@ namespace FarmTracker
                     new MasterDetailPage1MasterMenuItem { Id = 0, Title = "Homepage", Image="Home.png", TargetType = typeof(MainPage)},
                     new MasterDetailPage1MasterMenuItem { Id = 1, Title = "MyFarm", Image="tractor.png", TargetType = typeof(MyFarmTabbedPage)},
                     new MasterDetailPage1MasterMenuItem { Id = 2, Title = "Profile", Image="user.png", TargetType = typeof(ProfilePage)},
+                    new MasterDetailPage1MasterMenuItem { Id = 3, Title = "Login", Image="enter.png", TargetType = typeof(LoginPage)},
                 });
             }
 
